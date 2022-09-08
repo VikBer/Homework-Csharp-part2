fc04931546ecb4da9fe537b00ffdc4eeab443f3f
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-09-09 00:41:40 +0300

    ttt

--- Homework2.cs
+++ Homework2.cs
@@ -5,3 +5,8 @@ git checkout Lesson6task41 - Задача 41:
 
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3 */
+
+string M = "";
+
+Console.WriteLine("Введите строчку цифр через запятую");
+int.TryParse(Console.ReadLine(), out M);
