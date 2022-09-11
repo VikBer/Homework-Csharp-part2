d2d58da4d8fb48b626beb179d256a0b017cfc95e
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-09-11 21:30:51 +0300

    task solved

--- Homework2.cs
+++ Homework2.cs
@@ -1,39 +1,45 @@
 ﻿/*
         7. Задачи седьмого (7) семинара:
-git checkout Lesson7task47 - Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-0,5 7 -2 -0,2
-1 -3,3 8 -9,9
-8 7,8 -7,1 9
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
 
+17 -> такого числа в массиве нет
 */
 Console.Clear();
 
-Console.WriteLine("Введите количество строк массива");
-int m =  int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите количество столбцов массива");
-int n =  int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите первую координату искомой позиции");
+int a = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите значение минимального элемента диапазона для создания массива");
-int min =  int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите вторую координату искомой позиции");
+int b = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Введите значение максимального элемента диапазона для создания массива");
-int max =  int.Parse(Console.ReadLine()!);
+int[,] array = new int[new Random().Next(3,6), new Random().Next(3,6)];
 
-double[,] array = new double[m,n];
-
-void  FillArray (double[,] arr)
+void SearchingNumber (int x, int y)
+{
+        if (x < array.GetLength(0) && y < array.GetLength(1))
+                { 
+                        Console.Write($"\nЗначение искомого элемента = {array[x,y]} ");
+                }
+        else { Console.Write("Такого числа нет в массиве");}
+}
+void  FillArray (int[,] arr)
 {
         for (int i = 0; i < arr.GetLength(0); i++)
        {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                        arr[i,j] = Math.Round( new Random().NextDouble() + new Random().Next(min, max), 2);
+                        arr[i,j] = new Random().Next(0, 50);
                 }
        }
 }
 
-void PrintArray (double[,] arr)
+void PrintArray (int[,] arr)
 {
         for (int i = 0; i < arr.GetLength(0); i++)
        {
@@ -48,3 +54,4 @@ void PrintArray (double[,] arr)
 
 FillArray(array);
 PrintArray(array);
+SearchingNumber(a,b);
