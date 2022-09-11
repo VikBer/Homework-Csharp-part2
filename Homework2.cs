b03d9a231df547316b16444d3a6bec2bb45c48ad
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-09-11 23:18:51 +0300

    task solved

--- Homework2.cs
+++ Homework2.cs
@@ -1,40 +1,30 @@
 ﻿/*
         7. Задачи седьмого (7) семинара:
-Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
-и возвращает значение этого элемента или же указание, что такого элемента нет.
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
-
-17 -> такого числа в массиве нет
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
 Console.Clear();
 
-Console.WriteLine("Введите первую координату искомой позиции");
-int a = int.Parse(Console.ReadLine()!);
+int[,] array = new int[new Random().Next(3,6),new Random().Next(3,6) ];
 
-Console.WriteLine("Введите вторую координату искомой позиции");
-int b = int.Parse(Console.ReadLine()!);
+FillArray(array);
+PrintArray(array);
+Console.Write($"\n");
+ArithmeticMean(0);
 
-int[,] array = new int[new Random().Next(3,6), new Random().Next(3,6)];
-
-void SearchingNumber (int x, int y)
-{
-        if (x < array.GetLength(0) && y < array.GetLength(1))
-                { 
-                        Console.Write($"\nЗначение искомого элемента = {array[x,y]} ");
-                }
-        else { Console.Write("Такого числа нет в массиве");}
-}
 void  FillArray (int[,] arr)
 {
         for (int i = 0; i < arr.GetLength(0); i++)
        {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                        arr[i,j] = new Random().Next(0, 50);
+                        arr[i,j] = new Random().Next(0, 10);
                 }
        }
 }
@@ -45,13 +35,24 @@ void PrintArray (int[,] arr)
        {
                 for (int j = 0; j < arr.GetLength(1); j++)
                 {
-                  Console.Write($"{array[i,j]} ");
+                  Console.Write($"{arr[i,j]} ");
                 }
          Console.Write($"\n");
        }
        
 }
 
-FillArray(array);
-PrintArray(array);
-SearchingNumber(a,b);
+void ArithmeticMean (double aMe)
+{
+        Console.Write("Cреднее арифметическое каждого столбца: ");
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+                aMe = 0;
+                for (int j = 0; j < array.GetLength(0); j++)
+                        {
+                                aMe += array[j,i];
+                        }
+                aMe = Math.Round(aMe / array.GetLength(0),2);
+                Console.Write($"{aMe}; ");
+        }
+}
