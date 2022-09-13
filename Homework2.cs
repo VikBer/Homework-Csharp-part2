a88a2e10c80b3f8da95f1555ff0aa1d28ec35c17
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-09-13 18:48:09 +0300

    task solved

--- Homework2.cs
+++ Homework2.cs
@@ -1,33 +1,54 @@
 ﻿/*
-       Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
-       Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
 
-Массив размером 2 x 2 x 2
-66(0,0,0) 25(0,1,0)
-34(1,0,0) 41(1,1,0)
-27(0,0,1) 90(0,1,1)
-26(1,0,1) 55(1,1,1)
 */
 
 Console.Clear();
 
-int[,,] array = new int [ new Random().Next(2, 4), new Random().Next(2, 4), new Random().Next(2, 4)];
-
-
-int[,,] FillAndPrintArray(int[,,] arr)
+void Print (int[,] arr)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        for (int i = 0; i < arr.GetLength(0); i++)
         {
-           for (int k = 0; k < arr.GetLength(2); k++)
-            {
-                arr[i, j, k] = new Random().Next(10, 100);
-                Console.Write($"{arr[i,j,k]} ({i},{j},{k})");
-            }
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                        if (arr[i,j] < 10)
+                        Console.Write($"0{arr[i,j]} ");
+                        else Console.Write($"{arr[i,j]} ");
+                }
+        Console.Write("\n");             
         }
-       Console.Write("\n");
-    }
-    return arr;
 }
-FillAndPrintArray(array);
+
+
+int[,] arrayA = new int[4, 4];
+
+
+
+int[,] Spiral (int[,] arr)
+{
+        int temp = 1;
+        int i = 0;
+        int j = 0;
+
+        while (temp <= arrayA.GetLength(0) * arrayA.GetLength(1))
+        {
+        arrayA[i, j] = temp;
+        temp++;
+        if (i <= j + 1 && i + j < arrayA.GetLength(1) - 1)
+        j++;
+        else if (i < j && i + j >= arrayA.GetLength(0) - 1)
+        i++;
+        else if (i >= j && i + j > arrayA.GetLength(1) - 1)
+        j--;
+        else
+        i--;
+        }
+        return arr;
+}
+
+Print(Spiral(arrayA));
