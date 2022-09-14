c2ed1ee1af79b7110d8c2becd527114581abbdc4
Victor Berezovskiy <vik.berezovsky@yandex.ru>
2022-09-14 22:38:03 +0300

    task solved

--- Homework2.cs
+++ Homework2.cs
@@ -7,3 +7,23 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 */
 
+Console.Clear();
+Console.WriteLine("Введите значение N");
+int n = int.Parse(Console.ReadLine()!);
+
+int Recursion (int r)
+{
+        if (r <= 0) 
+                {
+                Console.Write("Введено не натуральное число "); 
+                return r;
+                }
+        if (r > 1) 
+        {
+                Console.Write($"{r}, ");
+                return Recursion(r-1);
+        }
+        return r;
+}
+
+Console.WriteLine(Recursion(n));
